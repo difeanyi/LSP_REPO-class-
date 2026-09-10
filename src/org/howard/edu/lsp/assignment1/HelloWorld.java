649b0b89a1a568d4dac6bfed92c5ec5bc7115580
David Ifeanyi <davidifeanyii2004@gmail.com>
2026-09-10 14:30:37 -0400

    added name to java doc

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,5 +1,8 @@
 package org.howard.edu.lsp.assignment1;
 
+/**
+ * @author David Ifeanyi
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
